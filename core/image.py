049158a25fb4e8af0531dac167dcdf2ea526aa3f
Santhosh <nodegeek@gmail.com>
2014-04-20 15:06:01 +0530

    first commit

--- core/image.py
+++ core/image.py
@@ -2,7 +2,7 @@ __author__ = 'Santhosh'
 from PIL import Image
 import StringIO
 
-def create_thumbnail(size,image_buffer,image_name, file_path ):
+def create_thumbnail(size, image_buffer, image_name, file_path ):
     img = Image.open(StringIO.StringIO(image_buffer))
     img.thumbnail(size, Image.ANTIALIAS)
     image_file = file_path+image_name
