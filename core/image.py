dfd91d54441f8de516ba5f26163a14382ef1291f
Santhosh <nodegeek@gmail.com>
2014-04-20 15:16:45 +0530

    first commit

--- core/image.py
+++ core/image.py
@@ -5,6 +5,6 @@ import StringIO
 def create_thumbnail(size, image_buffer, image_name, file_path ):
     img = Image.open(StringIO.StringIO(image_buffer))
     img.thumbnail(size, Image.ANTIALIAS)
-    image_file = file_path+image_name
+    image_file = file_path+image_name.lower()
     img.save(image_file, "JPEG")
     return image_file
